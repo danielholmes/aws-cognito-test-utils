7eb16a85b45601ac590a254cd30491bbeed8dd4d
Daniel Holmes <daniel@danielholmes.org>
2023-01-23 07:14:33 +1100

    More efficient typing

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,14 @@
 import { omit, partial } from "lodash-es";
-import { DefaultBodyType, MockedRequest, RestHandler } from "msw";
-import changePasswordHandler, {
-  ChangePasswordOptions,
-} from "./change-password";
-import confirmSignUpHandler, { ConfirmSignUpOptions } from "./confirm-sign-up";
+import changePasswordHandler from "./change-password";
+import confirmSignUpHandler from "./confirm-sign-up";
 import { BaseEndpointOptions } from "./create-handler";
 import {
   initiateAuthNewPasswordRequiredHandlers,
-  InitiateAuthNewPasswordRequiredOptions,
-  InitiateAuthNonConfirmedUserSignInHandlers,
   initiateAuthNonConfirmedUserSignInHandlers,
 } from "./initiate-auth";
-import resendConfirmationCodeHandler, {
-  ResendConfirmationCodeOptions,
-} from "./resend-confirmation-code";
+import resendConfirmationCodeHandler from "./resend-confirmation-code";
 
-type Handler = RestHandler<MockedRequest<DefaultBodyType>>;
-
-type CognitoHandlersFactory = {
-  readonly changePasswordHandler: (options: ChangePasswordOptions) => Handler;
-  readonly confirmSignUpHandler: (options: ConfirmSignUpOptions) => Handler;
-  readonly resendConfirmationCodeHandler: (
-    options: ResendConfirmationCodeOptions
-  ) => Handler;
-  readonly initiateAuthNonConfirmedUserSignInHandlers: (
-    options: InitiateAuthNonConfirmedUserSignInHandlers
-  ) => ReadonlyArray<Handler>;
-  readonly initiateAuthNewPasswordRequiredHandlers: (
-    options: InitiateAuthNewPasswordRequiredOptions
-  ) => ReadonlyArray<Handler>;
-};
-
-function createCognitoHandlersFactory(
-  baseOptions: BaseEndpointOptions
-): CognitoHandlersFactory {
+function createCognitoHandlersFactory(baseOptions: BaseEndpointOptions) {
   return {
     changePasswordHandler: partial(
       changePasswordHandler,
@@ -54,6 +29,8 @@ function createCognitoHandlersFactory(
     ),
   };
 }
+
+type CognitoHandlersFactory = ReturnType<typeof createCognitoHandlersFactory>;
 
 export type { CognitoHandlersFactory };
 export { createCognitoHandlersFactory };
